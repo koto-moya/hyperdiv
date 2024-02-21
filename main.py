022876381c563abb892adced0227a56ce3d6607c
koto-moya <kotogenshi@gmail.com>
2024-02-21 12:06:18 -0500

    figuring out the charts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hyperdiv as hd
+import numpy as np
 
 def main():
     hd.text("Hello, world")
@@ -6,6 +7,15 @@ def main():
     
     slider = hd.slider()
     hd.progress_bar(f"{slider.value}", value =slider.value)
+
+    button = hd.button("cos")
+    if button.clicked:
+        x = [float(x) for x in np.arange(0,10,.01)]
+        y = [float(np.cos(xs)) for xs in x]
+        data = list(zip(x,y))
+        hd.line_chart(data)
+
+
 
 if __name__ == "__main__":
     hd.run(main)
